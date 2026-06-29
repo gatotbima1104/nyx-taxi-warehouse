efb02143ef87789459488acf2ff0548c262529e7
muhamad gatot <gatotbima2002@gmail.com>
2026-06-29 10:52:59 +0700

    fix: gold_daily_summary

--- db/schemas/gold/01_daily_summary.sql
+++ db/schemas/gold/01_daily_summary.sql
@@ -2,7 +2,7 @@ CREATE TABLE gold.daily_summary AS
 SELECT
 	ttc.pickup_date,
 	COUNT(*) AS total_trips,
-	ROUND(SUM(ttc.total_amount) AS daily_revenue),
+	ROUND(SUM(ttc.total_amount), 2) AS daily_revenue,
 	ROUND(AVG(ttc.total_amount), 2) AS avg_daily_revenue,
 	ROUND(AVG(ttc.trip_distance), 2) AS avg_trip_distance,
 	ROUND(AVG(ttc.trip_duration_minutes), 2) AS avg_duration_minute
