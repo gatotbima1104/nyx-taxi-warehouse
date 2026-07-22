91a264802c906e389447af8d04d4d25f3655f35c
muhamad gatot <gatotbima2002@gmail.com>
2026-07-22 11:09:06 +0700

    fix: add check and constraint in silver

--- db/init/03_silver_transform.sql
+++ db/init/03_silver_transform.sql
@@ -18,7 +18,7 @@ CREATE TABLE silver.taxi_trips_cleaned (
     pickup_hour INTEGER NOT NULL,
     pickup_day_time TEXT NOT NULL,
     is_weekend BOOLEAN NOT NULL,
-    time_period TEXT NOT NULL,
+    time_period TEXT NOT NULL CHECK (time_period IN ('Late Night', 'Morning', 'Afternoon', 'Evening', 'Night')),
     pickup_borough TEXT NOT NULL,
     pickup_zone TEXT NOT NULL,
 
@@ -26,26 +26,20 @@ CREATE TABLE silver.taxi_trips_cleaned (
     dropoff_borough TEXT NOT NULL,
     dropoff_zone TEXT NOT NULL,
 
-    passenger_count INTEGER NOT NULL,
+    passenger_count INTEGER NOT NULL CHECK (passenger_count > 0),
 
-    trip_distance NUMERIC(10,2) NOT NULL,
-    trip_duration_minutes NUMERIC(10,2) NOT NULL,
+    trip_distance NUMERIC(10,2) NOT NULL CHECK (trip_distance > 0),
+    trip_duration_minutes NUMERIC(10,2) NOT NULL CHECK (trip_duration_minutes > 0),
 
     rate_code_id INTEGER NOT NULL,
-    store_and_fwd_flag TEXT NOT NULL,   
+    store_and_fwd_flag TEXT NOT NULL CHECK (store_and_fwd_flag IN ('Normal', 'Store and Forward', 'Unknown')),
     pickup_location_id INTEGER NOT NULL,
     dropoff_location_id INTEGER NOT NULL,
-    payment_type TEXT NOT NULL,
-    fare_amount NUMERIC(10,2) NOT NULL,
-    -- extra NUMERIC(10,2) NOT NULL,
-    -- mta_tax NUMERIC(10,2) NOT NULL,
-    tip_amount NUMERIC(10,2) NOT NULL,
-    -- tolls_amount NUMERIC(10,2),
-    -- improvement_surcharge NUMERIC(10,2),
-    total_amount NUMERIC(10,2),
-    -- congestion_surcharge NUMERIC(10,2),
-    -- airport_fee NUMERIC(10,2),   
-    -- cbd_congestion_fee NUMERIC(10,2),
+    payment_type TEXT NOT NULL CHECK (payment_type IN ('Credit Card', 'Cash', 'No Charge', 'Dispute', 'Unknown')),
+    
+    fare_amount NUMERIC(10,2) NOT NULL CHECK (fare_amount > 0),
+    tip_amount NUMERIC(10,2) NOT NULL CHECK (tip_amount >= 0),
+    total_amount NUMERIC(10,2) NOT NULL CHECK (total_amount > 0),
 
     CONSTRAINT fk_pickup
         FOREIGN KEY(pickup_location_id) 
@@ -53,7 +47,11 @@ CREATE TABLE silver.taxi_trips_cleaned (
 
     CONSTRAINT fk_dropoff
         FOREIGN KEY(dropoff_location_id) 
-        REFERENCES silver.taxi_zones(location_id)
+        REFERENCES silver.taxi_zones(location_id),
+
+    CONSTRAINT chk_pickup_hour CHECK (pickup_hour BETWEEN 0 AND 23),
+    CONSTRAINT chk_pickup_before_dropoff CHECK(pickup_datetime < dropoff_datetime)
+
 );
 
 CREATE TABLE silver.data_quality_issues (
@@ -84,7 +82,7 @@ CREATE TABLE silver.data_quality_issues (
     payment_type TEXT NOT NULL,
     fare_amount NUMERIC(10,2) NOT NULL,
     tip_amount NUMERIC(10,2) NOT NULL,
-    total_amount NUMERIC(10,2),
+    total_amount NUMERIC(10,2) NOT NULL,
     error_type TEXT NOT NULL,
 
     CONSTRAINT fk_pickup
